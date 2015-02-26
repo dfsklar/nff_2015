07e99c1a7b21bd28063926a32e143017572d7bea
dfsklar <dfsklar@gmail.com>
2015-02-25 18:11:02 -0800

    RESPONDENT-BY-SECTOR now correct

--- dataprep/extract.sh
+++ dataprep/extract.sh
@@ -1,1 +1,1 @@
-perl extract_final.pl < Data_Pull_29JAN2015.txt 3> extract_stderr.txt | tee result.tmp > ../webapp/realdata.json
+perl extract_2015.pl < Data_Pull_29JAN2015.txt 3> extract_stderr.txt | tee result.tmp > ../webapp/realdata.json
---
+++ dataprep/extract_2014.sh
@@ -0,0 +1,1 @@
+perl extract_final_2014.pl < 2014_survey_final_31MAR2014.txt | tee result.tmp > ../webapp/realdata.json
