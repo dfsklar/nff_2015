6f47064d91eb77c141780265b60270c350a6e424
root <root@li479-42.members.linode.com>
2015-03-04 13:26:21 -0500

    New way to compile-into-production on the linode box

--- compile.sh
+++ compile.sh
@@ -1,7 +1,2 @@
-/bin/rm -rf www
-harp compile webapp www
-gtar cvfz www_nff.taz www
-sftp nffusa@linode.sklardevelopment.com <<EOF
-put www_nff.taz
-put dataprep/extract_final.pl
-EOF
+/bin/rm -rf /home/nffusa/www
+harp compile webapp /home/nffusa/www
